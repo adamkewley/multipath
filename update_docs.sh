1951ba9fed684c58ab3d741b8673d75ca7b1d25b
Adam Kewley <akewley@ast.cam.ac.uk>
2018-04-11 14:08:03 +0100

    Updated doc build script (again)

--- update_docs.sh
+++ update_docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TMP_DIR=mktemp -d
+TMP_DIR=$(mktemp -d)
 
 pushd docs
 
@@ -8,11 +8,11 @@ make clean
 sphinx-apidoc -o source ..
 make html
 
-mv build/html/* ${TMP_DIR}
+cp cp -r build/html/* ${TMP_DIR}
 
 popd docs
 
 git checkout gh-pages
 rm -rf *
 touch .nojekyll
-mv ${TMP_DIR}/* .
+cp -r ${TMP_DIR}/* .
